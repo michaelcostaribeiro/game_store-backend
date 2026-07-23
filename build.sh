616166b4e0e8054283225326a4a9c7372efc597d
sleepyheadremote <azulciano57@gmail.com>
2026-07-23 20:00:42 -0300

    superuser creation added to build.sh

--- build.sh
+++ build.sh
@@ -6,3 +6,8 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
+
+
+if [ "$DJANGO_SUPERUSER_USERNAME" ]; then
+    python manage.py createsuperuser --noinput || true
+fi
